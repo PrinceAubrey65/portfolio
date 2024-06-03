ec5d55d1595e47718669ca125076b0f54f34cb41
nuwagabaprinceaubrey <nuwagabaprinceaubrey@gmail.com>
2024-06-03 11:39:57 +0000

    Hey

--- nav/nav.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
-
-
-hamburger.addEventListener('click', function(){
-    this.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-});
